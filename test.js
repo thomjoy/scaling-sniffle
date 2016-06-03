a51ffa5f9aeeccc17ebfa7f3e63613226992bba0
Thom Joy <thomjoy1984@gmail.com>
2016-06-03 18:15:49 +0100

    Adding questions

--- test.js
+++ test.js
@@ -40,31 +40,115 @@ var app = express();
   From: '+61481458770',
   ApiVersion: '2010-04-01'
 }
-
 */
+
+var users = {};
+var questions = {
+  stage1: {
+    q: "Hi, how are you? Would you like some (co): chocolate " + emoji.chocolate_bar +
+      " or (ap): an apple " + emoji.apple + "?",
+    validResponses: ['yes', 'no', 'co', 'chocolate', 'ap', 'apple', 'both'],
+    replies: {
+      'yes': 'Yes, what? (co): chocolate ' + emoji.chocolate_bar + ' or (ap): an apple ' + emoji.apple + '?',
+      'no': 'Ok. Never mind. Have a nice day!',
+      'co': "I'm a chocolate lover too. What do you want (tw): Twix, or (sn) Snickers?",
+      'chocolate': "I'm a chocolate lover too. What do you want (tw): Twix, or (sn) Snickers?",
+      'ap': 'Healthy choice! Do you want me to deliver it to your location: Hilton Sydney, 488 George Street, Sydney, NSW, 2000?',
+      'apple': 'Healthy choice! Do you want me to deliver it to your location: Hilton Sydney, 488 George Street, Sydney, NSW, 2000?',
+      'both': 'Clever ;) What kind of chocolate do you want? (tw): Twix or (sn): Snickers?'
+    },
+    invalidResponse: 'Stage 1, invalid Response'
+  },
+  stage2: {
+    q: ''
+    validResponses: ['tw', 'twix', 'sn', 'snickers'],
+    replies: {
+      'tw': 'Twix it is! Do you want (1): one or (2) two?',
+      'twix': 'Twix it is! Do you want (1): one or (2) two?',
+      'sn': 'My favourite! Do you want (1): one or (2) two?'
+    },
+    invalidResponse: "I'm sure that's yummy, but I only have (tw): Twix or (sn): Snickers..."
+  },
+  stage3: {
+    q: '',
+    validResponses: ['1', '2', 'one', 'two'],
+    replies: {
+      '1': 'Really? Ok then. Do you want me deliver it to Hilton Sydney, 448 George Street?',
+      'one': 'Really? Ok then. Do you want me deliver it to Hilton Sydney, 448 George Street?',
+      '2': 'Stupid question I know... Do you want me deliver it to Hilton Sydney, 448 George Street?',
+      'two': 'Stupid question I know... Do you want me deliver it to Hilton Sydney, 448 George Street?'
+    },
+    invalidResponse: "Nice try. You get 2. Do you want me deliver it to Hilton Sydney, 448 George Street?"
+  },
+  stage4: {
+    q: '',
+    validResponses: ['y', 'yes', 'ok', 'n', 'no'],
+    replies: {
+      'y': 'Cool. How do you want to pay? (not): not all at all, (cc): Your credit card (totally not secure, but hey, if you want to...)',
+      'yes': 'Cool. How do you want to pay? (not): not all at all, (cc): Your credit card (totally not secure, but hey, if you want to...)',
+      'ok': 'Cool. How do you want to pay? (not): not all at all, (cc): Your credit card (totally not secure, but hey, if you want to...)',
+      'n': "Well, that wasn't really a question. I'm just a prototype and can't deliver anywhere else yet. Do you still want your treats?",
+      'no': "Well, that wasn't really a question. I'm just a prototype and can't deliver anywhere else yet. Do you still want your treats?",
+    },
+    invalidResponse: "Well, that wasn't really a question. I'm just a prototype and can't deliver anywhere else yet. Do you still want your treats?",
+  },
+  stage5: {
+    q: '',
+    validResponses: ['not', 'cc', 'credit card', 'yes', 'no'],
+    replies: {
+      'not': 'I thought so. Your treats will be with you shortly! Was great chatting to you. Have a nice day!'
+      'cc': "I can't believe you did that! Don't send your CC details! Your treats will be with you shortly. Have a nice day!",
+      'credit card': "I can't believe you did that! Don't send your CC details! Your treats will be with you shortly. Have a nice day!",
+      'yes': '',
+      'no': 'Ok. Never mind. Have a nice day!'
+    },
+    invalidResponse: 'I thought so. Your treats will be with you shortly! Was great chatting to you. Have a nice day!'
+  }
+};
 
 app.get('/inbound', function(req, res) {
 
   var params = req.query;
   var messageBody = req.query.Body.toLowerCase();
+  var userId = req.query.From;
+
+  var stage;
   var response;
 
   console.log(messageBody);
   console.log(req.query);
 
-  // Parse incoming message
-  switch (messageBody) {
-    case "chocolate":
-      response = "Great job! You chose: " + emoji.chocolate_bar;
-      break;
-    case "chips":
-      response = "Nice one! You chose " + emoji.fries;
-      break;
-    default:
-      response = "I don't know what you chose, so you get: " + emoji.poop;
-      break;
+  // Look up the user by number, and work out which stage of the script they are at
+  if (! users[userId]) {
+    users[userId] = {};
+    users[userId].stage = 0;
   }
 
+  stage = user[userId].stage;
+  var stringStage = 'stage' + stage;
+
+  // beginning interaction
+  if (stage === 0) {
+    switch(messageBody) {
+      case "hello":
+        response = "Hi, how are you? Would you like some (co): chocolate " + emoji.chocolate_bar +
+          " or (ap): an apple " + emoji.apple + "?";
+        break;
+      default:
+        response = "Sorry, I'm a bot and still learning. At the moment, I can only offer " +
+          "(co): chocolate " + emoji.chocolate_bar + " or (ap): an apple " + emoji.apple + "?";
+        break;
+    }
+  }
+  else {
+    // parse request
+    if (questions[stringStage].validResponses.indexOf(messageBody) !== -1 ) {
+      response = questions[stringStage].replies[messageBody];
+    }
+    else {
+      response = questions[stringStage].invalidResponse;
+    }
+  }
 
   var twiml = new twilio.TwimlResponse();
   twiml.message(response);
